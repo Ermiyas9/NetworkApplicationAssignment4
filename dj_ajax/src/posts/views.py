d301d3415bac57ccfe55288efccfc9d9b413bff7
Ermiyas <egulti4128@conestogac.on.ca>
2024-04-13 17:39:53 -0400

    added authentication for each pages after inserting user permmission code.:

--- dj_ajax/src/posts/views.py
+++ dj_ajax/src/posts/views.py
@@ -5,12 +5,13 @@ from .forms import PostForm
 from profiles.models import Profile
 from .utils import action_permission
 from django.contrib.auth.decorators import login_required
-
-
 # Create your views here.
+
+@login_required
 def post_list_and_create(request):
     form = PostForm(request.POST or None)
-    
+    #qs = Post.objects.all()
+
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         if form.is_valid():
             author = Profile.objects.get(user=request.user)
@@ -19,15 +20,27 @@ def post_list_and_create(request):
             instance.save()
             return JsonResponse({
                 'title': instance.title,
-                'body': instance.body,
-                'author': instance.author.user.username,
-                'id': instance.id
+                'body' : instance.body,
+                'author' : instance.author.user.username,
+                'id': instance.id,
             })
     context = {
         'form': form,
+
     }
     return render(request, 'posts/main.html', context)
 
+@login_required
+def post_detail(request, pk):
+    obj = Post.objects.get(pk=pk)
+    form = PostForm()
+    context = {
+        'obj': obj,
+        'form': form,
+    }
+    return render(request,'posts/detail.html', context)
+
+@login_required
 def load_post_data_view(request, num_posts):
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         visible = 3
@@ -49,9 +62,24 @@ def load_post_data_view(request, num_posts):
             }
             data.append(item)
         return JsonResponse({'data':data[lower:upper], 'size': size})
-    
+
+@login_required
+def post_detail_data_view(request, pk):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        obj = Post.objects.get(pk=pk)
+        data = {
+            'id':obj.id,
+            'title': obj.title,
+            'body': obj.body,
+            'author' : obj.author.user.username,
+            'logged_in': request.user.username,
+        }
+        return JsonResponse({'data':data})
+    return redirect('posts:main-board')
+
+@login_required
 def like_unlike_post(request):
-     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         pk = request.POST.get('pk')
         obj = Post.objects.get(pk=pk)
         if request.user in obj.liked.all():
@@ -61,8 +89,10 @@ def like_unlike_post(request):
             liked = True
             obj.liked.add(request.user)
         return JsonResponse({'liked' : liked, 'count' : obj.like_count})
-    
-    
+    return redirect('posts:main-board')
+
+@login_required
+@action_permission
 def update_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
@@ -77,7 +107,8 @@ def update_post(request, pk):
     })
     return redirect('posts:main-board')
 
-
+@login_required
+@action_permission
 def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
@@ -86,4 +117,11 @@ def delete_post(request, pk):
     #return JsonResponse({'msg': 'access denied - ajax only'})
     return redirect('posts:main-board')
 
-
+@login_required
+def image_upload_view(request):
+    if request.method == 'POST':
+        img = request.FILES.get('file')
+        new_post_id = request.POST.get('new_post_id')
+        post = Post.objects.get(id=new_post_id)
+        Photo.objects.create(image=img, post=post)
+    return HttpResponse
