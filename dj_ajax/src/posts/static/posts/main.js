f6ce317b043e7f2909fb0b2afe978dc0440ca197
Ermiyas <egulti4128@conestogac.on.ca>
2024-04-12 22:12:36 -0400

    created an end-box with id='end-box' to display Load more button or 'No posts have been added yet' notification based on post availability. Implemented logic to show 'No more posts to load' when all posts are displayed.

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,54 +1,66 @@
 console.log('hello worlds')
 
-const helloWorldBox = document.getElementById('HelloWorld')
 const postsBox = document.getElementById('post-box')
 const spinnerBox = document.getElementById('spinner-box')
+const loadBtn = document.getElementById('load-btn')
+const endBox = document.getElementById('end-box')
 
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success: function (response) {
-        console.log('success', response.text)
-        helloWorldBox.textContent = response.text
-    },
-    error: function (error) {
-        console.log('error', error)
-    }
-})
+let visible = 3
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function (response) {
-        console.log(response)
-        const data = response.data 
-        setTimeout(()=>{
-            spinnerBox.classList.add('not-visible')
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML += `
-                <div class="card mb-2">
-                    <div class="card-body">
-                        <h5 class="card-title">${el.title}</h5>
-                        <p class="card-text">${el.body}</p>
-                    </div>
+const getData=()=> {
 
-                    <div class="card-footer">
-                        <div class="row">
-                            <div class= "col-1">
-                                <a href="#" class="btn btn-primary">Details</a>
-                            </div>
-                            <div class="col-1">
-                                <a href="#" class="btn btn-primary">Like</a>
-                            </div>
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function (response) {
+            console.log(response)
+            const data = response.data 
+            setTimeout(()=>{
+                spinnerBox.classList.add('not-visible')
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML += `
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>
                         </div>
-                    </div
-              </div>
-                `
-            });
-        }, 1000)
-    },
-    error: function (error) {
-        console.log(error)
-    }
+    
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class= "col-2">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-2">
+                                    <a href="#" class="btn btn-primary">Like</a>
+                                </div>
+                            </div>
+                        </div
+                  </div>
+                    `
+                });
+            }, 100)
+            console.log(response.size)
+            if(response.size===0){
+                endBox.textContent='No posts added yet...'
+            }
+            else if (response.size <= visible){
+                loadBtn.classList.add('non-visible')
+                endBox.textContent = 'No post too load...'
+            }
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    })    
+}
+
+loadBtn.addEventListener('click', ()=>{
+    
+    spinnerBox.classList.remove('not-visible')
+    visible += 3 
+
+    getData()
 })
+
+getData()
