530a7ec1c0a8039f2af58507b07730e0eccbb65a
Ermiyas <egulti4128@conestogac.on.ca>
2024-04-13 19:35:10 -0400

    i have fixed recursion error in URL patterns and fixed 404 error for profiles/my/ endpointt

--- dj_ajax/src/post_proj/urls.py
+++ dj_ajax/src/post_proj/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for post_proj project.
+URL configuration for posts_proj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls', namespace='posts'))
+    path('', include('posts.urls', namespace='posts')),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root =settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dj_ajax/src/profiles/urls.py
+++ dj_ajax/src/profiles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import my_profile_view
+
 app_name = 'profiles'
 
 urlpatterns = [
