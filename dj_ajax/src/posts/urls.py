cb0c3f7ce4390455af8804783b3aa2be1dbc4dd8
Ermiyas <egulti4128@conestogac.on.ca>
2024-04-13 17:47:04 -0400

    Implemented JavaScript code to dynamically fetch post data and manage UI elements based on user authorization, including enabling update and delete buttons for the post's author. Added detail.js file for this functionality.

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -1,18 +1,25 @@
 from django.urls import path
-from.views import(
+from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
-    
-    hello_world_view,
+    post_detail,
+    post_detail_data_view,
+    delete_post,
+    update_post,
+    image_upload_view
 )
 
-app_name ='posts'
+app_name = 'posts'
 
-urlpatterns =[
-    path('', post_list_and_create,name='main-board'),
+urlpatterns = [
+    path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
-    path('data/<int:num_posts>/',load_post_data_view,name='posts-data'),
-    path('hello-world/',hello_world_view, name='hello-world'),
+    path('upload/', image_upload_view, name='image-upload'),
+    path('<pk>/', post_detail, name='post-detail'),
+    path('<pk>/update/', update_post, name='post-update'),
+    path('<pk>/delete/', delete_post, name='post-delete'),
+    path('data/<int:num_posts>/', load_post_data_view,name='posts-data'),
+    path('<pk>/data/',post_detail_data_view,name='post-detail-data'),
     
 ]
